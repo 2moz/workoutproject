f23a6316e313f08e352b13703d06cd3348a55598
tomas <tomas@tomass-MacBook-Pro.local>
2018-08-20 14:29:51 -0500

    added component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: {
+      name: 'Tomas flores',
+      emai: 'tomas@gmail.com',
+      workouts: [
+        {
+          wourkoutName: 'benchpress',
+          reps: '8',
+          sets: '4',
+          restTime: '5',
+          day: '2'
+        }
+      ]
+    }
+  };
+
   render() {
-    return <div className="App" />;
+    const { users } = this.state;
+    return <div className="App">{console.log(users.workouts)}</div>;
   }
 }
 
